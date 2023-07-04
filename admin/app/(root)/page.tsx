1b160f0c0f2a87bd21a6ee7132ff04a6c6fea1ac
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-04 16:50:08 -0400

    Added Clerk Auth
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ admin/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div className='p-4'>
+      <Button size='default' variant={'destructive'}>
+        Click Me
+      </Button>
+    </div>
+  );
+}
