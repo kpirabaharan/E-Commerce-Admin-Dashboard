f8a260d5ada28de138085eef398f09be707b0126
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-24 00:22:20 -0400

    Created 'Create Store' Modal
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- components/modals/StoreModal.tsx
+++ components/modals/StoreModal.tsx
@@ -1,13 +1,43 @@
 'use client';
 
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import useStoreModal from '@/hooks/useStoreModal';
 
 import { Modal } from '@/components//ui/modal';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: 'Name of atleast one character required' }),
+});
+
 const StoreModal = () => {
   const { isOpen, onClose } = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    // TODO: Create Store
+  };
 
   return (
     <Modal
@@ -17,14 +47,29 @@ const StoreModal = () => {
       onClose={onClose}
     >
       <div className='flex flex-col gap-y-4'>
-        <p className='font-bold'>Name</p>
-        <Input placeholder='E-Commerce' />
-        <div className='flex flex-row justify-end gap-x-2'>
-          <Button variant={'outline'} onClick={onClose}>
-            Cancel
-          </Button>
-          <Button>Continue</Button>
-        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder='E-Commerce' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className='flex items-center justify-end pt-4 gap-x-2 w-full'>
+              <Button variant={'outline'} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button type='submit'>Confirm</Button>
+            </div>
+          </form>
+        </Form>
       </div>
     </Modal>
   );
