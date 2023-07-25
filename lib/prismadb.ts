fe668a69abc46447bff5a525778d2182737e6667
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-25 17:47:48 -0400

    Created PrismaDB Lib File
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ lib/prismadb.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prismadb = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') {
+  globalThis.prisma = prismadb;
+}
+
+export default prismadb;
