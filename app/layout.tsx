f6bbcbffc8ec55875ee3db276ceb30c25827f838
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-27 22:18:37 -0400

    Added Toasts Package and Provider to App
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 
 import ModalProvider from '@/providers/ModalProvider';
+import ToastProvider from '@/providers/ToastProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang='en'>
         <body className={inter.className}>
+          <ToastProvider />
           <ModalProvider />
           {children}
         </body>
