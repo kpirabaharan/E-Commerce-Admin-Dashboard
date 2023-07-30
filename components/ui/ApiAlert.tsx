72f66419b11b33c1e9fd9bd3a9390b02d6774712
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-30 17:04:52 -0400

    Made Icon Inline with Badge
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- components/ui/ApiAlert.tsx
+++ components/ui/ApiAlert.tsx
@@ -33,11 +33,12 @@ export const ApiAlert = ({
 
   return (
     <Alert>
-      <ServerIcon size={16} />
-      <AlertTitle className='flex flex-row items-center gap-x-2'>
+      <ServerIcon className='h-4 w-4' />
+      <AlertTitle className='flex flex-row items-center gap-x-2 h-4'>
         {title}
         <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
       </AlertTitle>
+
       <AlertDescription className='mt-4 flex items-center justify-between'>
         <code
           className='relative rounded bg-muted px-[0.3rem] py-[0.2rem] 
