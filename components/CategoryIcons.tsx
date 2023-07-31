89755e6b665c82e256b387918b74aaf11cb8a43a
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-30 21:25:52 -0400

    Show Skeleton While Loading
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- components/CategoryIcons.tsx
+++ components/CategoryIcons.tsx
@@ -3,6 +3,7 @@
 import { useIcons } from '@/hooks/useIcons';
 
 import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 
 interface CategoryIconsProps {
   icon: string;
@@ -14,18 +15,22 @@ export const CategoryIcons = ({ icon, setIcon }: CategoryIconsProps) => {
 
   return (
     <>
-      {iconList.map((btn) => (
-        <Button
-          suppressHydrationWarning
-          key={btn.value}
-          type={'button'}
-          size={'icon'}
-          variant={icon === `${btn.value}` ? 'default' : 'outline'}
-          onClick={() => setIcon(btn.value)}
-        >
-          {<btn.icon size={24} />}
-        </Button>
-      ))}
+      {iconList.length === 0
+        ? [...Array(22)].map((e, ind) => (
+            <Skeleton className='h-10 w-10 rounded-md' key={ind} />
+          ))
+        : iconList.map((btn) => (
+            <Button
+              suppressHydrationWarning
+              key={btn.value}
+              type={'button'}
+              size={'icon'}
+              variant={icon === `${btn.value}` ? 'default' : 'outline'}
+              onClick={() => setIcon(btn.value)}
+            >
+              {<btn.icon size={24} />}
+            </Button>
+          ))}
     </>
   );
 };
