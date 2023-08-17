a9b781a32c123e11b4ff7714eeb5140e9ebd04e2
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-08-17 01:04:00 -0400

    Modified ENV Key for Production
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- lib/aws-client.ts
+++ lib/aws-client.ts
@@ -2,7 +2,7 @@ import S3 from 'aws-sdk/clients/s3';
 
 const s3 = new S3({
   apiVersion: '2006-03-01',
-  accessKeyId: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.AWS_PUBLIC_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.REGION,
   signatureVersion: 'v4',
