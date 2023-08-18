0d9e9f5a38dfdc32d73241e9959a99db76f86a61
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-08-18 01:09:09 -0400

    Fixed Hydration Issue on ThemeButton
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- components/ThemeButton.tsx
+++ components/ThemeButton.tsx
@@ -1,12 +1,23 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import { SunIcon, MoonIcon } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 
 const ThemeButton = () => {
   const { theme, setTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return <Skeleton className='h-8 w-8 rounded-full' />;
+  }
 
   return (
     <Button
