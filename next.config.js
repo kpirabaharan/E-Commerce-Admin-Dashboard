95c3b47ad749d6cef09603c5ae14cb4418f63c0f
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-08-17 23:38:58 -0400

    Added CORS Headers to Next Config
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- next.config.js
+++ next.config.js
@@ -6,6 +6,26 @@ const nextConfig = {
       'ecommerce-admin-kpirabaharan-products.s3.amazonaws.com',
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
