ced4c05a1450a73fb8dbc9089571fb649cc3ee71
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-08-25 19:16:43 -0400

    Webhook File Created
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,8 @@
+import Stripe from 'stripe';
+import { headers } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+import stripe from '@/lib/stripe';
+import prismadb from '@/lib/prismadb';
+
+export const POST = async () => {};
