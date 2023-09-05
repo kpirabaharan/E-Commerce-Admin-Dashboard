5981520562e42c3fa3c2a87eae7785ceae661809
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-09-04 23:20:06 -0400

    Test Account Sign In
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,13 @@
 import SignIn from '@/app/(auth)/components/SignIn';
+import TestSignIn from '@/app/(auth)/components/TestSignIn';
 
 const SignInPage = () => {
-  return <SignIn />;
+  return (
+    <div className='flex flex-col gap-y-8 items-center justify-center'>
+      <SignIn />
+      <TestSignIn />
+    </div>
+  );
 };
 
 export default SignInPage;
---
+++ app/(auth)/components/TestSignIn.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { MouseEvent } from 'react';
+import { redirect } from 'next/navigation';
+import { useSignIn } from '@clerk/nextjs';
+
+import { Button } from '@/components/ui/button';
+
+const TestSignIn = () => {
+  const { isLoaded, signIn } = useSignIn();
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  const testSignInWithEmailCode = async () => {
+    const emailAddress = 'john+clerk_test@example.com';
+    const signInResp = await signIn.create({ identifier: emailAddress });
+    const { emailAddressId } = signInResp.supportedFirstFactors.find(
+      (ff) =>
+        ff.strategy === 'email_code' && ff.safeIdentifier === emailAddress,
+    )! as any;
+
+    await signIn.prepareFirstFactor({
+      strategy: 'email_code',
+      emailAddressId: emailAddressId,
+    });
+
+    const attemptResponse = await signIn.attemptFirstFactor({
+      strategy: 'email_code',
+      code: '424242',
+    });
+
+    if (attemptResponse?.status == 'complete') {
+      redirect('/');
+    } else {
+      console.log('Login failed. Please try again later.');
+    }
+  };
+
+  return (
+    <Button
+      onClick={async (e: MouseEvent) => {
+        e.preventDefault();
+        await testSignInWithEmailCode();
+      }}
+    >
+      Sign In Anonymously to Test Admin Dashboard
+    </Button>
+  );
+};
+
+export default TestSignIn;
