f550b3052f27bd2ed1ca9ed7dd28bfe2465209ea
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-09-08 21:25:08 -0400

    Get Stock Count By Number of Total Products
    Not Product Count
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- actions/getStockCount.ts
+++ actions/getStockCount.ts
@@ -1,9 +1,14 @@
 import prismadb from '@/lib/prismadb';
 
 export const getStockCount = async (storeId: string) => {
-  const stockCount = await prismadb.product.count({
+  const products = await prismadb.product.findMany({
     where: { storeId, isArchived: false },
   });
+
+  const stockCount = products.reduce(
+    (total, product) => total + product.amount,
+    0,
+  );
 
   return stockCount;
 };
